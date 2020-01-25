2b80f4bfba0066af7e0e9397c5bee79566818e7a
daniel.meneses <daniel.meneses@cuemby.com>
2020-01-25 09:39:55 -0500

    - install axios - make proxy with axios and express to fetch courses data - fetch courses data from proxi using fetch api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
 $(function () {
-    console.log('he we go!')
+
+    var coursesObj,  coursesList;
+    getCourses();
+    
+    function getCourses(name = '') {        
+        fetch('/courses')        
+            .then(resp => {
+                return resp.json()
+            })
+            .then(jsonResp => {
+                console.log(jsonResp)
+                coursesObj = jsonResp
+                coursesList = jsonResp.items
+            })
+    }
 })
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const api = require('./api');
 var app = express();
 var path = require('path');
 
@@ -7,6 +8,25 @@ app.use(express.static(__dirname + '/assets'))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get('/courses', async (req, res) => {
+    let data = {};
+    const url = `/courses?orderBy=popularity+desc&expand=provider&limit=24&profession=&subjectAreaCode=&state=&provider=&name=`
+    await api.get(url)
+        .then(resp => {
+            if (resp.status === 200) {
+                data = resp.data
+            }
+        })
+        .catch(error => {
+            data = {
+                error: true,
+                message: 'There was an error fetching data.'
+            }
+        })
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+})
 
 app.listen(3000, () => {
     console.log('Running on port 3000')
